555cea0ed6755c8fdc549bf0eba2637cadb522df
Vladimir Logachev <doit@keemail.me>
2019-09-01 17:44:41 +0700

    solve some more

--- stepik/app/Main.hs
+++ stepik/app/Main.hs
@@ -4,7 +4,6 @@ import qualified Stepik2_4_3
 
 main :: IO ()
 main = do
-  print Stepik2_4_3.example1
-  print Stepik2_4_3.example2
-  print Stepik2_4_3.example3
-  print Stepik2_4_3.example4
+   Stepik2_4_3.askPasswordExample
+   return ()
+   
--- stepik/src/Stepik2_4_3.hs
+++ stepik/src/Stepik2_4_3.hs
@@ -1,10 +1,17 @@
 module Stepik2_4_3 where
 
+import           Control.Applicative
+import           Control.Monad
+import           Control.Monad.IO.Class     (liftIO)
 import           Control.Monad.Trans        (lift)
 import           Control.Monad.Trans.Except
+import           Control.Monad.Trans.Maybe
 import           Control.Monad.Trans.State
 import           Control.Monad.Trans.Writer
+
+import           Data.Char                  (isNumber, isPunctuation)
 import           Data.Either                (lefts)
+import           Data.Foldable              (traverse_)
 import           Data.Monoid
 
 data Tile
@@ -67,3 +74,92 @@ example3 = waysToDie Poisoned map1 3 (4, 2)
 
 example4 = waysToDie Poisoned map1 4 (4, 2)
   --  13
+
+newtype PwdError =
+  PwdError String
+  deriving (Show)
+
+instance Semigroup PwdError where
+  a <> b = a
+
+instance Monoid PwdError where
+  mempty = PwdError ""
+
+type PwdErrorMonad = ExceptT PwdError IO
+
+askPassword :: PwdErrorMonad ()
+askPassword = do
+  liftIO $ putStrLn "Enter your new password:"
+  value <- msum $ repeat getValidPassword
+  liftIO $ putStrLn "Storing in database..."
+
+getValidPassword :: PwdErrorMonad String
+getValidPassword = do
+  s <- liftIO getLine
+  validate s
+
+validate :: String -> PwdErrorMonad String
+validate s
+  | length s < 8 = printError "Incorrect input: password is too short!"
+  | not $ any isNumber s = printError "Incorrect input: password must contain some digits!"
+  | not $ any isPunctuation s = printError "Incorrect input: password must contain some punctuation!"
+  | otherwise = return s
+
+printError :: String -> PwdErrorMonad String
+printError err = do
+  lift $ putStrLn err
+  throwE $ PwdError err
+
+askPasswordExample = runExceptT askPassword
+  {-
+  runExceptT askPassword
+  Enter your new password:
+  qwerty
+  Incorrect input: password is too short!
+  qwertyuiop
+  Incorrect input: password must contain some digits!
+  qwertyuiop123
+  Incorrect input: password must contain some punctuation!
+  qwertyuiop123!!!
+  Storing in database...
+  -}
+
+data ReadError
+  = EmptyInput
+  | NoParse String
+  deriving (Show)
+
+-- моя реализация работает без изменений
+tryRead :: (Read a, Monad m) => String -> ExceptT ReadError m a
+tryRead [] = except $ Left EmptyInput
+tryRead str =
+  except $
+  case reads str of
+    ((x, ""):_) -> Right x
+    _           -> Left $ NoParse str
+
+data Tree a
+  = Leaf a
+  | Fork (Tree a) a (Tree a)
+  
+--instance Foldable Tree where
+
+--стоит ли закладывать order в инстанс?
+-- заново реализовать, потому что другие конструкторы.
+
+treeSum :: Tree a -> (Maybe ReadError, Integer)
+treeSum t =
+  let (err, s) = runWriter . runExceptT $ traverse_ go t
+   in (maybeErr err, getSum s)
+  where
+    maybeErr :: Either ReadError () -> Maybe ReadError
+    maybeErr = either Just (const Nothing)
+
+go :: String -> ExceptT ReadError (Writer (Sum Integer)) ()
+go = undefined
+
+inOrderExample1 = treeSum $ Fork (Fork (Leaf "1") "2" (Leaf "oops")) "15" (Leaf "16")
+  --(Just (NoParse "oops"),3)
+
+inOrderExample2 = treeSum $ Fork (Fork (Leaf "1") "2" (Leaf "0")) "15" (Leaf "16")
+  --(Nothing,34)
